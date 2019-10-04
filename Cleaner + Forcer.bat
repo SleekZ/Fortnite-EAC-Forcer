4ed8d9a937fffc92db4a93f2265daab7fc6cfea9
MrSleekZ <40346672+MrSleekZ@users.noreply.github.com>
2019-10-04 21:59:42 +1000

    Add files via upload

---
+++ Cleaner + Forcer.bat
@@ -0,0 +1,54 @@
+@shift /0
+@shift /0
+@echo off
+title Forcer + Cleaner for FN by MrSleekZ
+color 74
+taskkill /f /im FortniteClient-Win64-Shipping_EAC.exe
+taskkill /f /im FortniteClient-Win64-Shipping.exe
+taskkill /f /im FortniteClient-Win64-Shipping_BE.exe
+taskkill /f /im FortniteLauncher.exe
+taskkill /f /im EpicGamesLauncher.exe
+reg delete "HKEY_LOCAL_MACHINE\Hardware\Description\System\BIOS" /v BIOSVendor /f
+reg delete "HKEY_LOCAL_MACHINE\Hardware\Description\System\BIOS" /v BIOSReleaseDate /f
+reg delete "HKEY_LOCAL_MACHINE\Hardware\Description\System\BIOS" /v SystemManufacturer /f
+reg delete "HKEY_LOCAL_MACHINE\Hardware\Description\System\BIOS" /v SystemProductName /f
+reg delete "HKEY_LOCAL_MACHINE\Hardware\Description\System\BIOS" /v SystemManufacturer /f
+reg delete "HKEY_LOCAL_MACHINE\Hardware\Description\System\CentralProcessor\0" /v ProcessorNameString /f
+reg delete "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control" /v SystemStartOptions /f
+reg delete "HKEY_CURRENT_USER\Software\Epic Games" /f
+REG ADD HKLM\SYSTEM\CurrentControlSet\Control\ComputerName\ComputerName /v ComputerName /t REG_SZ /d Norc%random% /f
+REG ADD HKLM\SYSTEM\CurrentControlSet\Control\ComputerName\ActiveComputerName /v ComputerName /t REG_SZ /d Norc%random% /f
+REG ADD HKLM\SYSTEM\HardwareConfig /v LastConfig /t REG_SZ /d {be%random%} /f
+REG ADD HKLM\SYSTEM\CurrentControlSet\Control\IDConfigDB\Hardware" "Profiles\0001 /v HwProfileGuid /t REG_SZ /d {fefefee%random%-%random%-%random%-%random%} /f
+REG ADD HKLM\SYSTEM\CurrentControlSet\Control\IDConfigDB\Hardware" "Profiles\0001 /v GUID /t REG_SZ /d {fefefe%random%-%random%-%random%-%random%} /f
+REG ADD HKLM\SOFTWARE\Microsoft\Windows" "NT\CurrentVersion /v BuildGUID /t REG_SZ /d Norc%random% /f
+REG ADD HKLM\SOFTWARE\Microsoft\Windows" "NT\CurrentVersion /v RegisteredOwner /t REG_SZ /d Norc%random% /f
+REG ADD HKLM\SOFTWARE\Microsoft\Windows" "NT\CurrentVersion /v RegisteredOrganization /t REG_SZ /d Norc%random% /f
+REG ADD HKLM\SOFTWARE\Microsoft\Cryptography /v GUID /t REG_SZ /d Norc%random%-%random%-%random%-%random% /f
+REG ADD HKLM\SOFTWARE\Microsoft\Cryptography /v MachineGuid /t REG_SZ /d hello%random%-%random%-%random%-%random% /f
+REG ADD HKLM\SOFTWARE\Microsoft\Windows" "NT\CurrentVersion /v ProductId /t REG_SZ /d %random%-%random%-%random%-%random% /f
+REG ADD HKLM\SOFTWARE\Microsoft\Windows" "NT\CurrentVersion /v InstallDate /t REG_SZ /d %random% /f
+REG ADD HKLM\SYSTEM\CurrentControlSet\Control\SystemInformation /v ComputerHardwareId /t REG_SZ /d {randomd%random%-%random%-%random%-%random%} /f
+date 08-22-19
+
+set /p id=What do you want?(eac/be):
+ 
+IF %id%==be cd %systemdrive%\Program Files (x86)\Epic Games\Launcher\Engine\Binaries\Win32
+IF %id%==be start epicgameslauncher.exe
+IF %id%==be TIMEOUT /T 3
+IF %id%==be date 08-19-19
+
+IF %id%==BE cd %systemdrive%\Program Files (x86)\Epic Games\Launcher\Engine\Binaries\Win32
+IF %id%==BE start epicgameslauncher.exe
+IF %id%==BE TIMEOUT /T 3
+IF %id%==BE date 08-19-19
+
+IF %id%==eac cd %systemdrive%\Program Files (x86)\Epic Games\Launcher\Engine\Binaries\Win32
+IF %id%==eac start epicgameslauncher.exe
+IF %id%==eac TIMEOUT /T 3
+IF %id%==eac date 08-20-19
+
+IF %id%==EAC cd %systemdrive%\Program Files (x86)\Epic Games\Launcher\Engine\Binaries\Win32
+IF %id%==EAC start epicgameslauncher.exe
+IF %id%==EAC TIMEOUT /T 3
+IF %id%==EAC date 08-20-19
