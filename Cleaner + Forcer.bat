9c84dfb2368817350585564b1b27885746432e1e
MrSleekZ <40346672+MrSleekZ@users.noreply.github.com>
2020-01-14 13:33:08 +1100

    Update Cleaner + Forcer.bat

--- Cleaner + Forcer.bat
+++ Cleaner + Forcer.bat
@@ -1,3 +1,28 @@
+@echo off
+:-------------------------------------
+  IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:-------------------------------------- 
 @shift /0
 @shift /0
 @echo off
@@ -29,26 +54,35 @@ REG ADD HKLM\SOFTWARE\Microsoft\Cryptography /v MachineGuid /t REG_SZ /d hello%r
 REG ADD HKLM\SOFTWARE\Microsoft\Windows" "NT\CurrentVersion /v ProductId /t REG_SZ /d %random%-%random%-%random%-%random% /f
 REG ADD HKLM\SOFTWARE\Microsoft\Windows" "NT\CurrentVersion /v InstallDate /t REG_SZ /d %random% /f
 REG ADD HKLM\SYSTEM\CurrentControlSet\Control\SystemInformation /v ComputerHardwareId /t REG_SZ /d {randomd%random%-%random%-%random%-%random%} /f
-date 08-22-19
+date 1-10-2020
+
+echo Please make sure that you date/time format is (Month/Day/Year)
 
 set /p id=What do you want?(eac/be):
+
 
 IF %id%==be cd %systemdrive%\Program Files (x86)\Epic Games\Launcher\Engine\Binaries\Win32
 IF %id%==be start epicgameslauncher.exe
 IF %id%==be TIMEOUT /T 3
-IF %id%==be date 12/19/19
+IF %id%==be date 19/19/19
+IF %id%==be date 1/10/20
 
 IF %id%==BE cd %systemdrive%\Program Files (x86)\Epic Games\Launcher\Engine\Binaries\Win32
 IF %id%==BE start epicgameslauncher.exe
 IF %id%==BE TIMEOUT /T 3
-IF %id%==BE date 12/19/19
+IF %id%==BE date 19/19/19
+IF %id%==BE date 1/10/20
 
 IF %id%==eac cd %systemdrive%\Program Files (x86)\Epic Games\Launcher\Engine\Binaries\Win32
 IF %id%==eac start epicgameslauncher.exe
 IF %id%==eac TIMEOUT /T 3
-IF %id%==eac date 12/19/19
+IF %id%==eac date 19/19/19
+IF %id%==eac date 1/10/20
 
 IF %id%==EAC cd %systemdrive%\Program Files (x86)\Epic Games\Launcher\Engine\Binaries\Win32
 IF %id%==EAC start epicgameslauncher.exe
 IF %id%==EAC TIMEOUT /T 3
-IF %id%==EAC date 12/19/19
+IF %id%==EAC date 19/19/19
+IF %id%==EAC date 1/10/20
+
+pause
